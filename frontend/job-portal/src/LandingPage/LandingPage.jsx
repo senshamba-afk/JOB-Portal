bd322f86d01fc4ca305975e900dad0155760c65e
falak679 <falakfatima9709@gmail.com>
2026-02-11 02:20:04 +0530

    footer added

--- frontend/job-portal/src/LandingPage/LandingPage.jsx
+++ frontend/job-portal/src/LandingPage/LandingPage.jsx
@@ -3,15 +3,17 @@ import Analytics from './components/Analytics'
 import Header from './components/Header'
 import Hero from './components/Hero'
 import Features from './components/Features'
+import Footer from './components/Footer'
 import { Briefcase } from 'lucide-react'
 
 const LandingPage = () => {
   return (
-    <div className='min-h-screen mb-[100vh]'>
+    <div className='min-h-screen'>
       <Header />
       <Hero />
       <Features/>
       <Analytics />
+      <Footer />
     </div>
   )
 }
