532beef765496942fdd0bad8550c75c2038cfd9e
Himanshu kumar <himanshuece4747@gmail.com>
2026-02-11 16:31:29 +0530

    nothing to add in main directry

--- frontend/job-portal/src/LandingPage/LandingPage.jsx
+++ frontend/job-portal/src/LandingPage/LandingPage.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import Analytics from './components/Analytics'
 import Header from './components/Header'
-import Hero from './components/Hero'
-import Features from './components/Features'
+// import Hero from './components/Hero'
+// import Features from './components/Features'
 import { Briefcase } from 'lucide-react'
 
 const LandingPage = () => {
   return (
     <div className='min-h-screen mb-[100vh]'>
       <Header />
-      <Hero />
+      {/* <Hero />
       <Features/>
-      <Analytics />
+      <Analytics /> */}
     </div>
   )
 }
